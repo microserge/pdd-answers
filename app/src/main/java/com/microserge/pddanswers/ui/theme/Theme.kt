47ee9b525fa554ea8f1ab7f3964015080a2cd043
DESKTOP-B4EOPP3\MicroSerg <belarus1552@gmail.com>
2025-02-28 14:45:15 +0300

    initial commit

--- app/src/main/java/com/microserge/pddanswers/ui/theme/Theme.kt
+++ app/src/main/java/com/microserge/pddanswers/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.microserge.pddanswers.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
